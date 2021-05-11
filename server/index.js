3407722575dec865728dd9836dea457d440c2879
inakins <53452799+inakins@users.noreply.github.com>
2021-05-11 16:01:56 -0400

    Refactor: deleted force true from db sync

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = require("./app");
 
 const init = async () => {
   try {
-    await db.sync({ force: true });
+    await db.sync();
     // start listening (and create a 'server' object representing our server)
     app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
   } catch (ex) {
