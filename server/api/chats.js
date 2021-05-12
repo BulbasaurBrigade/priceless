066bc7c6156e0b010d08dde6ad47b9fd06e72bfc
Meredith Cornelius <meredith.cornelius@gmail.com>
2021-05-12 11:44:52 -0400

    feat: get all chats for particular user

--- server/api/chats.js
+++ server/api/chats.js
@@ -1,27 +1,27 @@
 const router = require("express").Router();
-const { Op } = require("sequelize")
+const { Op } = require("sequelize");
 const {
-    models: { Chat, Post, User },
-} = require("../db")
-module.exports = router
+  models: { Chat, Post, User },
+} = require("../db");
+module.exports = router;
 
 router.get("/users/:id", async (req, res, next) => {
-    try {
-        const chats = await Post.findAll({ 
-            include: {model: User, as: 'recipient' },
-            // where: {
-            //     [Op.or]: [
-            //         {
-            //             RecipientId: req.params.id
-            //         },
-            //         {
-            //             '$post.posterId$': req.params.id
-            //         }
-            //     ]
-            // } 
-        })
-        res.send(chats)
-    } catch (err) {
-        next(err)
-    }
-})
+  try {
+    const chats = await Post.findAll({
+      include: { model: User, as: "recipient", required: true },
+      where: {
+        [Op.or]: [
+          {
+            posterId: req.params.id,
+          },
+          {
+            "$recipient.id$": req.params.id,
+          },
+        ],
+      },
+    });
+    res.send(chats);
+  } catch (err) {
+    next(err);
+  }
+});
