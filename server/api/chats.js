eb6e7b6ced5122bfd204cb84d0af47bd0b8ff636
Meredith Cornelius <meredith.cornelius@gmail.com>
2021-05-12 12:08:38 -0400

    feat: get all messages for single chat

--- server/api/chats.js
+++ server/api/chats.js
@@ -5,17 +5,18 @@ const {
 } = require("../db");
 module.exports = router;
 
-router.get("/users/:id", async (req, res, next) => {
+//GET /api/users/:userId/chats
+router.get("/", async (req, res, next) => {
   try {
     const chats = await Post.findAll({
       include: { model: User, as: "recipient", required: true },
       where: {
         [Op.or]: [
           {
-            posterId: req.params.id,
+            posterId: req.params.userId,
           },
           {
-            "$recipient.id$": req.params.id,
+            "$recipient.id$": req.params.userId,
           },
         ],
       },
@@ -23,5 +24,17 @@ router.get("/users/:id", async (req, res, next) => {
     res.send(chats);
   } catch (err) {
     next(err);
+  }
+});
+
+//GET /api/users/:userId/chats/:chatId/messages
+router.get("/:chatId/messages", async (req, res, next) => {
+  try {
+    const chatAndMessages = await Chat.findByPk(req.params.chatId, {
+      include: { model: Message, order: [["createdAt", "DESC"]] },
+    });
+    res.send(chatAndMessages);
+  } catch (error) {
+    next(error);
   }
 });
