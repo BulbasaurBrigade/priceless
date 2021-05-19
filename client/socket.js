d85e702bd7c9dec23c48bb94f5b951ead39d5b0c
Sarah Zerod <szerod@gmail.com>
2021-05-19 14:59:32 -0400

    Chore: comment out debugging logger

--- client/socket.js
+++ client/socket.js
@@ -4,8 +4,8 @@ import { io } from 'socket.io-client';
 const socket = io();
 
 // debugging func to log all events received by client
-socket.onAny((event, ...args) => {
-  console.log(event, args);
-});
+// socket.onAny((event, ...args) => {
+//   console.log(event, args);
+// });
 
 export default socket;
