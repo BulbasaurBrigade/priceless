d3d114e987188c945b69555b579c4654a95beefe
Meredith Cornelius <meredith.cornelius@gmail.com>
2021-05-22 14:22:27 -0400

    chore: location added to user db model

--- client/components/myAccount/EditProfile.js
+++ client/components/myAccount/EditProfile.js
@@ -5,21 +5,28 @@ import UserInfoForm from "../UserAuth/UserInfoForm";
 
 class EditProfile extends React.Component {
   render() {
+    const { displayName, updateUser } = this.props;
     return (
       <div className="form-container">
-        <h2>Edit your profile</h2>
-        <UserInfoForm />
+        <h2>Edit your display name and location</h2>
+        <UserInfoForm submit={updateUser} displayName={displayName} />
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  return {};
+  return {
+    displayName: state.auth.displayName,
+    lat: state.auth.latitude,
+    lng: state.auth.longitude,
+  };
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {};
+const mapDispatchToProps = (dispatch, { history }) => {
+  return {
+    updateUser: (user) => dispatch(editUser(user, history)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditProfile);
--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,18 +1,21 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const axios = require('axios');
+const Sequelize = require("sequelize");
+const db = require("../db");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const axios = require("axios");
 
 //const SALT_ROUNDS = 5;
 
-const User = db.define('user', {
+const User = db.define("user", {
   displayName: {
     type: Sequelize.STRING,
   },
   isAdmin: {
     type: Sequelize.BOOLEAN,
     defaultValue: false,
+  },
+  location: {
+    type: Sequelize.TEXT,
   },
   latitude: {
     type: Sequelize.FLOAT,
