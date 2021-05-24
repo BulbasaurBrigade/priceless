440c6075033d962a0f9614d7357dee84fa26b0a1
Leora Douek <leorakurtz@gmail.com>
2021-05-24 19:54:17 -0400

    chore: add linkedIn

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -8,18 +8,30 @@ const Footer = () => {
         <div className="developer">
           <img src="https://robohash.org/peasant" />
           <p>Ina Agee</p>
+          <a href="https://www.linkedin.com/in/inaagee/">
+            <img className="linkedIn-logo" src="/LinkedIn.png" />
+          </a>
         </div>
         <div className="developer">
           <img src="meredith.png" />
           <p>Meredith Cornelius</p>
+          <a href="https://www.linkedin.com/in/meredith-cornelius/">
+            <img className="linkedIn-logo" src="/LinkedIn.png" />
+          </a>
         </div>
         <div className="developer">
           <img src="/Leora.jpg" />
           <p>Leora Douek</p>
+          <a href="https://www.linkedin.com/in/leoradouek/">
+            <img className="linkedIn-logo" src="/LinkedIn.png" />
+          </a>
         </div>
         <div className="developer">
           <img src="/ZerodSarah_blurred.png" />
           <p>Sarah Zerod</p>
+          <a href="https://www.linkedin.com/in/sarahzerod/">
+            <img className="linkedIn-logo" src="/LinkedIn.png" />
+          </a>
         </div>
       </div>
     </div>
