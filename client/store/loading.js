2ba79a0eefb8c494a3610dd8f94176a241eb4566
Sarah Zerod <szerod@gmail.com>
2021-05-25 14:39:01 -0400

    super careful

--- client/store/loading.js
+++ client/store/loading.js
@@ -2,7 +2,11 @@
 /* eslint-disable no-underscore-dangle */
 import { SET_AUTH } from './auth';
 import { CREATE_POST } from './posts';
-import { SET_POSTFORM_ERROR, SET_USERPROFILE_ERROR } from './error';
+import {
+  SET_POSTFORM_ERROR,
+  SET_PREVIEWLOCATION_ERROR,
+  SET_USERPROFILE_ERROR,
+} from './error';
 
 // Action Types
 const IS_LOADING = 'IS_LOADING';
@@ -35,6 +39,7 @@ export default (state = initialLoadingState, action) => {
     case CREATE_POST:
     case SET_POSTFORM_ERROR:
     case SET_USERPROFILE_ERROR:
+    case SET_PREVIEWLOCATION_ERROR:
     case SET_AUTH:
       return noLoading;
     default:
