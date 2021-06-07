1ea918b9042a3da06d92c1be81335ae350b4ce66
Sarah Zerod <szerod@gmail.com>
2021-06-07 14:21:58 -0400

    Chore: adds comments to associations

--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,4 @@
-//this is the access point for all things database related!
+// this is the access point for all things database related!
 
 const db = require('./db');
 
@@ -9,14 +9,18 @@ const Message = require('./models/message');
 const Post = require('./models/post');
 const PostImage = require('./models/postImage');
 
-//associations could go here!
+// Associations below
 
+// Associating users to posts as the poster
 User.hasMany(Post, { as: 'postedItem', foreignKey: 'posterId' });
 Post.belongsTo(User, { as: 'poster', foreignKey: 'posterId' });
 
+// Associating users to posts as the recipient
 User.hasMany(Post, { as: 'recipientItem', foreignKey: 'recipientId' });
 Post.belongsTo(User, { as: 'recipient', foreignKey: 'recipientId' });
 
+// Creating records of users expressing interest in requesting an item
+// through the LotteryTicket through table
 Post.belongsToMany(User, {
   as: 'requester',
   foreignKey: 'postId',
@@ -28,31 +32,29 @@ User.belongsToMany(Post, {
   through: LotteryTicket,
 });
 
+// Associating users to specific chats as either recipients or posters
 User.hasMany(Chat, { as: 'posterChat', foreignKey: 'posterId' });
 User.hasMany(Chat, { as: 'recipientChat', foreignKey: 'recipientId' });
 Chat.belongsTo(User, { as: 'poster', foreignKey: 'posterId' });
 Chat.belongsTo(User, { as: 'recipient', foreignKey: 'recipientId' });
 
+// Each chat will be created when a new combination of poster and recipient
+// is created with a specific post
+// That is expressed by chats belonging to a post because that will be the best
+// constant to get all previous and current chats around a specific listing
 Post.hasMany(Chat);
 Chat.belongsTo(Post);
 
-// User.belongsToMany(Post, {
-//   as: "post",
-//   foreignKey: "recipientId",
-//   through: Chat,
-// });
-// Post.belongsToMany(User, {
-//   as: "recipient",
-//   foreignKey: "postId",
-//   through: Chat,
-// });
-
+// Messages are tied to the chat they belong to
 Message.belongsTo(Chat);
 Chat.hasMany(Message);
 
+// And the user who sent them
 Message.belongsTo(User);
 User.hasMany(Message);
 
+// Finally, images are kept in their own table and associated to the post they
+// are tied to
 Post.hasMany(PostImage);
 PostImage.belongsTo(Post);
 
