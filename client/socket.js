a8bd7ec876c9404bf5824cb84ee58157254536c1
Sarah Zerod <szerod@gmail.com>
2021-06-07 15:02:36 -0400

    Chore: adds client side socket comments

--- client/socket.js
+++ client/socket.js
@@ -8,18 +8,22 @@ import { _updateChat } from './store/singleChat';
 const socket = io();
 
 // debugging func to log all events received by client
-socket.onAny((event, ...args) => {
-  console.log(event, args);
-});
+// socket.onAny((event, ...args) => {
+//   console.log(event, args);
+// });
 
+// On receiving a new message, dispatch it to the store
+// Because the user will only receive message events if they're in the relevant socket room
 socket.on('new message', ({ message }) => {
   store.dispatch(_newMessage(message));
 });
 
+// Dispatches an update post action with the new post information
 socket.on('post status update', ({ post }) => {
   store.dispatch(_updatePost(post));
 });
 
+// If the user belongs to the chat, dispatch an event updating it
 socket.on('updated chat', ({ chat }) => {
   const { chats } = store.getState();
   if (chats.some((localChat) => localChat.id === chat.id)) {
