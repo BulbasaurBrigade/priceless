7b4ccc612b58db3e747b16ebe6328e9fe75e3174
Sarah Zerod <szerod@gmail.com>
2021-06-07 15:27:23 -0400

    Chore: adds route comments and allows not signed in users acccess to the FAQ

--- client/routes.js
+++ client/routes.js
@@ -1,10 +1,9 @@
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Login, Signup } from './components/UserAuth/AuthForm';
 import Admin from './components/Admin';
-import socket from './socket';
 
 import Homepage from './components/Homepage';
 import MainContainer from './components/MainContainer';
@@ -24,10 +23,15 @@ class Routes extends Component {
   render() {
     const { isLoggedIn, hasDisplayName, isAdmin } = this.props;
 
+    // If a user is logged in but has not display name
+    // The website won't let them do anything except for update their profile, or log out
     if (isLoggedIn && !hasDisplayName) return <MoreInfo />;
 
     return (
       <>
+        {/* All users have ability to view homepage, all posts, and FAQ */}
+        {/* Not logged in users can log in or sign up */}
+        {/* Logged in users have additional access to their account, ability to add posts and view chats */}
         {isLoggedIn ? (
           <Switch>
             <Route exact path="/" component={Homepage} />
@@ -39,6 +43,7 @@ class Routes extends Component {
             <Route path="/mypost/:id" component={EditPost} />
             <Route path="/profile" component={EditProfile} />
             <Route path="/faq" component={FAQ} />
+            {/* Only gives a user access to admin routes if they are in fact an admin */}
             {isAdmin && <Route path="/admin/:view?" component={Admin} />}
             <Redirect to="/" />
           </Switch>
@@ -48,24 +53,11 @@ class Routes extends Component {
             <Route path="/posts" component={MainContainer} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
+            <Route path="/faq" component={FAQ} />
             <Redirect to="/login" />
           </Switch>
         )}
       </>
-      // <div>
-      //   {isLoggedIn ? (
-      //     <Switch>
-      //       <Route path="/home" component={Homepage} />
-      //       <Redirect to="/home" />
-      //     </Switch>
-      //   ) : (
-      //     <Switch>
-      //       <Route path='/' exact component={ Login } />
-      //       <Route path="/login" component={Login} />
-      //       <Route path="/signup" component={Signup} />
-      //     </Switch>
-      //   )}
-      // </div>
     );
   }
 }
